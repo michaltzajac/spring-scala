02a1906e7b67f6a4546769513f70ad1a4feb1aea
mzajac <mzajac@objectivity.co.uk>
2014-11-06 15:12:45 +0100

    .

--- src/main/scala/michalz/springscala/akka/AkkaService.scala
+++ src/main/scala/michalz/springscala/akka/AkkaService.scala
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
 
-/**
- */
 @Service
 class AkkaService {
   
